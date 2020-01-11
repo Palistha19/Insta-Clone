819f4f758cdfa93cacb7082cddbf16be5268219a
Palistha12 <palisthashakya19@gmail.com>
2020-01-11 11:12:36 +0545

    "Delete"

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def delete_photos(req,ID):
+    photos=Photos.objects.get(id=ID)
+    photos.delete()
+    return redirect("photos_home")
+
