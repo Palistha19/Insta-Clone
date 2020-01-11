317fcd6914da197f6d144b05065e73372d290199
Palistha12 <palisthashakya19@gmail.com>
2020-01-11 11:09:38 +0545

    "Update"

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def get_update_photos(req,ID):
+    photos=Photos.objects.get(id=ID)
+    context={
+        "photos":photos
+    }
+    return render(req,"update_photos.html",context=context)
+
+def post_update_photos(req,ID):
+    label=req.POST["label"]
+    caption=req.POST["caption"]
+
+    image=req.FILES['photo']
+    fs=FileSystemStorage()
+    filename=fs.save(image.name,image)
+    url=fs.url(filename)
+
