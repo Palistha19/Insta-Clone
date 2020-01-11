f40f7f9d1f419f72dabf9af087c61f88ef678123
Palistha12 <palisthashakya19@gmail.com>
2020-01-11 11:15:31 +0545

    Search

--- photos/views.py
+++ photos/views.py
@@ -8,3 +8,12 @@ def delete_photos(req,ID):
     photos.delete()
     return redirect("photos_home")
 
+def get_photos_home(req):
+    all_photos=Photos.objects.all()
+    if 'query_lable' in req.GET:
+        all_photos=Photos.objects.filter(label=req.GET['query_lable'])
+
+    context={
+        "photos":all_photos
+    }
+    return render(req,'photos_home.html',context=context)
