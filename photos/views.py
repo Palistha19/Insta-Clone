cf5f109b31bbda86bf4fdba455d6cc20b0915157
Palistha12 <palisthashakya19@gmail.com>
2020-01-11 10:45:36 +0545

    "Create and Read"

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_add_photos(req):
+    label=req.POST["label"]
+    caption=req.POST["caption"]
+
+    image=req.FILES['photo']
+    fs=FileSystemStorage()
+    filename=fs.save(image.name,image)
+    url=fs.url(filename)
+
+
+    new_photos=Photos(label=label,caption=caption,photo=url)
+    new_photos.save()
+
+    return redirect('photos_home')
+
+def get_photos_home(req):
+    all_photos=Photos.objects.all()
+    if 'query_lable' in req.GET:
+        all_photos=Photos.objects.filter(label=req.GET['query_lable'])
+
+    context={
+        "photos":all_photos
+    }
+    return render(req,'photos_home.html',context=context)
